41b0d91d2af944609db383de2c9387028ac0fc8f
adeonita <adeonita.sousa@gmail.com>
2019-12-26 18:38:18 -0300

    Ajustes

--- Associacao/index.php
+++ Associacao/index.php
@@ -1,13 +1,12 @@
 <?php
-//Este é um exemplo de AGREGAÇÃO, onde a classe product recebe a classe fornecedor como parâmetro
-//Porém ambas independem uma da outra
+/*Este é um exemplo de AGREGAÇÃO, onde a classe product recebe a classe fornecedor como parâmetro
+Porém ambas independem uma da outra*/
 
 require_once('product.class.php');
 require_once('fornecedor.class.php');
 
 $fornecedor = new Fornecedor( '1234', 'Agatismo', 'Salvador', "Rua Venezuela n383 - E paripe" );
 $product = new Product('Bela calcinha confeccionada em renda', 69.69, 10, 'aj345', $fornecedor ); 
-
 
 echo "Código do produto: ". $product->code . "<br>";
 echo "Descrição do produto: " . $product->description . "<br>";
