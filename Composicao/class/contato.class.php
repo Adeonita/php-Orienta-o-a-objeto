b161387aeda8bd5eda2e18c3b35d58dcaf2f1115
adeonita <adeonita.sousa@gmail.com>
2019-12-27 20:54:29 -0300

    Implementaçao de composiçao

--- Composicao/class/contato.class.php
+++ Composicao/class/contato.class.php
@@ -1,3 +1,15 @@
 <?php
-    
+    class Contact{
+        private $nome;
+        private $telefone;
+
+        public function setContact($nome, $telefone){
+            $this->nome = $nome;
+            $this->telefone =  $telefone;
+        }
+
+        public function getContact(){
+            return "Nome: {$this->nome}, Telefone: {$this->telefone}";
+        }
+    }
 ?>
--- Composicao/class/fornecedor.class.php
+++ Composicao/class/fornecedor.class.php
@@ -8,12 +8,21 @@ class Fornecedor{
     public $corporateName;
     public $city;
     public $address;
+    public $contact;
 
     public function __construct($code, $corporateName, $city, $address){
         $this->code = $code;
         $this->corporateName = $corporateName;
         $this->city = $city;
         $this->address = $address;
+        $this->contact = new Contact();
+    }
+     
+    public function addContact($nome, $telefone){ //Adiciona um novo contato
+        $this->contact->setContact($nome, $telefone);  //Chama a função setContact do objeto contact
     }
 
+    public function returnContact(){  //Retorna os dados de um contato
+        return $this->contact->getContact();  //Chama a função getContact do objeto contact
+    }
 }
