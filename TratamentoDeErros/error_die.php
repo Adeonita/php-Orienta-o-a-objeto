61ddb5ca7c488eb86195ddab4b90f09cbdf49f0f
adeonita <adeonita.sousa@gmail.com>
2019-12-30 00:41:33 -0300

    Tratamento de erros error_die

---
+++ TratamentoDeErros/error_die.php
@@ -0,0 +1,16 @@
+<?php 
+    //Aborta a execução se houver erro
+    function open($file = NULL){
+        if(!$file){
+            die('É necessário inserir o parâmetro com o nome do arquivo');
+        }
+        if(!file_exists($file)){
+            die('Arquivo inexistente');
+        }
+        if(!$retorno = @file_get_contents($file)){
+            die('Impossível ler o arquivo');
+        }
+    }
+    open(); //Erro 01
+    open('teste'); //Erro 02
+?>
