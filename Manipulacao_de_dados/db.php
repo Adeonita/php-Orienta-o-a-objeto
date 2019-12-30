55b468f5af52014fb0411d93e5c1a744a319ad4e
adeonita <adeonita.sousa@gmail.com>
2019-12-30 20:46:22 -0300

    Create funcion createDataBase()

--- Manipulacao_de_dados/db.php
+++ Manipulacao_de_dados/db.php
@@ -1,21 +1,42 @@
-<?php
-    $connection = mysqli_connect('mysql', 'root', 'tiger');
-    $nameDb = 'book';
-    $query = "CREATE DATABASE {$nameDb}";
-    $chek = mysqli_select_db($connection, $nameDb);  //Checa se um banco existe
+<?php #158
+    error_reporting(E_ALL ^ E_WARNING); 
 
-    if(!$connection){
-        die('Não é possível conectar-se ao banco'.mysqli_error());
-    }else{
-        if($chek == FALSE){ //Se o banco não existir  
-            if(mysqli_query($connection, $query)){ //Abro uma conexão e crio o banco através da query
-                echo 'Criado com sucesso';
-            }else{
-                echo "O banco {$nameDb} não pôde ser criado";
-            }
+    class CONNECTIONERROR extends Exception{};
+    class DUPLICATEDATABASE extends Exception{};
+ 
+    function createDataBase(){
+        $connection;
+        $connection = mysqli_connect('mysql', 'root', 'tiger');
+        $nameDb = 'testando';
+        $query = "CREATE DATABASE {$nameDb}"; //or CREATE IF NOT EXIST DATABASE
+        $chek = mysqli_select_db($connection, $nameDb);  //Checa se um banco existe
+       
+        if(!$connection){
+            throw new CONNECTIONERROR('Não é possível conectar-se ao banco'.mysqli_error());
         }else{
-            echo "O banco {$nameDb} já existe";
+            if($chek == FALSE){ //Se o banco não existir  
+                if(mysqli_query($connection, $query)){ //Abro uma conexão e crio o banco através da query
+                    echo 'Criado com sucesso';
+                    return TRUE;
+                }
+            }else{
+                throw new DUPLICATEDATABASE("O banco {$nameDb} já existe");
+                return FALSE;
+            }
         }
+
+        $endConnection = mysqli_close($connection);
+        /*if($endConnection){
+            echo '<br>Finalizando a conexão...';
+        } */
+    }
+
+    try{
+        createDataBase();
+    }catch(CONNECTIONERROR $exception){
+        echo $exception->getMessage();
+    }catch(DUPLICATEDATABASE $exception){
+        echo $exception->getMessage();
     }
     
     
