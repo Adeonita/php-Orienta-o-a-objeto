170b588133a3c99970ceb902237b0865afeff0f3
adeonita <adeonita.sousa@gmail.com>
2019-12-29 23:48:37 -0300

    Objetos Dinamicos

---
+++ ObjetosDinamicos/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    //Permite instanciar objetos dinamicamente sem a criação da classe ou a instanciação do objeto
+    
+    $ade->name = 'Adeonita';
+    $ade->idade = 23;
+    $ade->profissao = 'Development';
+    print_r($ade);
+?>
