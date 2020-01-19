4f23c45acef53be23274fab2872e767a97e04fe0
adeonita <adeonita.sousa@gmail.com>
2020-01-19 17:20:56 -0300

    Teste com metodos get e set

---
+++ Orientacao_a_objeto-introducao/metodos.php
@@ -0,0 +1,66 @@
+<?php
+    declare(strict_types = 1);
+
+    class Product{
+        private $description;
+        private $amount;
+        private $price;
+
+        public function setDescription(string $description){
+            if(is_string($description)){
+                $this->description = $description;
+            }
+        }
+
+        public function setAmount(int $amount){
+            if(is_numeric($amount) AND $amount > 0){
+                $this->amount = $amount;
+            }
+        }
+
+        public function setPrice(float $price){
+            if(is_numeric($price) AND $price > 0){
+                $this->price = $price;
+            }
+        }
+
+        public function getDescription(): string{
+            return $this->description;
+        }
+
+        public function getAmount(): int{
+            return $this->amount;
+        }
+
+        public function getPrice(): float{
+            return $this->price;
+        }
+        public function aumentarEstoque(int $unidades){
+            if(is_numeric($unidades) AND $unidades >= 0){
+                $this->amount = $this->amount + $unidades;
+            }
+        }
+
+        public function diminuirEstoque(int $unidades){
+            if(is_numeric($unidades) AND $unidades > 0){
+                $this->amount = $this->amount - $unidades;
+            }
+        }
+
+        public function reajustarPreco(float $percentual){
+            if(is_numeric($percentual) AND $percentual >= 0){
+                $this->price = $this->price * (1 + ($percentual/100));
+            }
+        }
+    } 
+
+
+    $p1 = new Product();
+    $p1->setDescription('Chocolate');
+    $p1->setAmount(10);
+    $p1->setPrice(8.0);
+
+    print "Description: {$p1->getDescription()} <br>" . PHP_EOL;
+    print "Estoque: {$p1->getAmount()} <br>" . PHP_EOL;
+
+?>
