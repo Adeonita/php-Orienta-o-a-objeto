d9204b540ad05728462b8762a536001a77040ea1
adeonita <adeonita.sousa@gmail.com>
2020-03-24 22:57:07 -0300

    Conlusao parcial do nivel 6

---
+++ Do_procedural_ao_orientado_a_objetos/nivel_6/class/Person.class.php
@@ -0,0 +1,70 @@
+<?php
+    declare(strict_types=1);
+
+    class Person{
+
+        private static $conn;
+
+        /**
+         *  Método estático que cria a conexão com o banco de dados 
+         * @return conn: Conexão com o banco de dados 
+         */
+        public static function getConnection(){
+            if(empty(self::$conn)){  //Se a variavel local de conexão estiver vazia carrego os dados nela
+                $connection = parse_ini_file('config/database.ini');  //Carrego as configurações do banco de dados
+                $host = $connection['host'];
+                $dbName = $connection['dbname'];
+                $username = $connection['username'];
+                $password = $connection['password'];
+                self::$conn = new PDO("mysql:host={$host};dbname={$dbName};", $username, $password);
+            }
+            return self::$conn;
+        }
+
+        /**
+         * Método que salva uma pessoa no banco de dados conforme os dados passados
+         * Se existir um id ela Atualiza, caso contrário Insere
+         * 
+         * @param person
+         * @return void
+         */
+        public static function save($person){  //Recebe um objeto do tipo person
+            $conn = self::getConnection();
+            if(empty($person['id'])){
+                $result = $conn->query('SELECT max(id) as next FROM  pessoas'); //Seleciona o último id da tabela pessoas
+                $row = $result->fetch();
+                $person['id'] = (int) $row['next']+1;
+                $query = "INSERT INTO pessoas (id, nome, endereco, bairro, telefone, email, id_cidade)
+                                VALUES (:id, :nome, :endereco, :bairro, :telefone, :email, :id_cidade)";
+            }else{
+                $query = "UPDATE pessoas SET nome = :nome,
+                                             endereco = :endereco,
+                                             bairro = :bairro,
+                                             telefone = :telefone,
+                                             email = :email,
+                                             id_cidade = :id_cidade
+                                        WHERE id = :id";
+            }
+            $result = $conn->prepare($query); //Prepara a query para ser executada
+            $result->execute([    //'traduz'  :atributo para o seu real valor 
+                            ':id' => $person['id'],
+                            ':nome' => $person['nome'],
+                            ':endereco' => $person['endereco'],
+                            ':bairro' => $person['bairro'],
+                            ':telefone' => $person['telefone'],
+                            ':email' => $person['email'],
+                            ':id_cidade' => $person['id_cidade']
+                    ]);
+        }
+
+        public static function showPerson(int $id){
+            $conn = self::getConnection();
+            $query = 'SELECT * FROM pessoas WHERE id = :id';
+            $result = $conn->prepare($query);
+            $result->query([
+                ':id' => $id
+            ]);
+            return $result->fetch();            
+        }
+    }
+?>
---
+++ Do_procedural_ao_orientado_a_objetos/nivel_6/index.php
@@ -0,0 +1,12 @@
+<?php
+    require_once('class/Person.class.php');
+
+    try{
+        $person = $_POST;  //Pego todos os dados que vem via post
+        $person =Person::showPerson(16);
+        print_r($person);
+        //Person::save($person);  //Chamo o método estático save para inserir ou atualizar o usuário no banco 
+    }catch(Exception $e){
+        print $e->getMessage();
+    }
+?>
