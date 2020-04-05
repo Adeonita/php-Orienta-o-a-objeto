9f096501f1b6c9518a29fbe0d035d971d931a62a
adeonita <adeonita.sousa@gmail.com>
2020-04-05 20:39:04 -0300

    Instancia de City para testes

--- Do_procedural_ao_orientado_a_objetos/nivel_7/index.php
+++ Do_procedural_ao_orientado_a_objetos/nivel_7/index.php
@@ -1,6 +1,11 @@
 <?php
     require_once('class/PersonForm.class.php');
+    require_once('class/City.class.php');
 
     $form = new PersonForm();
     $form->show();
+
+    $form = new City();
+    
+    
 ?>
