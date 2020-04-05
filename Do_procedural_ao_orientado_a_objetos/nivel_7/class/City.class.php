ee71a1ba46f2d8c43f934cefb0d91dea369e0d83
adeonita <adeonita.sousa@gmail.com>
2020-04-04 21:07:12 -0300

    Implementation of method find all

--- Do_procedural_ao_orientado_a_objetos/nivel_7/class/City.class.php
+++ Do_procedural_ao_orientado_a_objetos/nivel_7/class/City.class.php
@@ -1,13 +1,22 @@
 <?php
 
     class City{
+
+        use Connection{
+            Connection::__construct as private __traitConstruct;
+        }
         
         public function  __construct(){
-
+            $this__traitConstruct();
         }
 
-        public static function all(){
+        public static function findAll(){
+            $query = 'SELECT nome FROM cidades';
+            $statement = $this->connection->prepare($query);
+            $statement->execute();
+            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
             
+            return $result;
         }
     }
 ?>
