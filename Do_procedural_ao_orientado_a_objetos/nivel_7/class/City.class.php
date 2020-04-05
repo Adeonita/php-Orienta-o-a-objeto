aa1dd0e5e2905280c15e2e2a253d609d24595b96
adeonita <adeonita.sousa@gmail.com>
2020-04-05 20:38:27 -0300

    Correçao e finalizaçao do metodo findAll

--- Do_procedural_ao_orientado_a_objetos/nivel_7/class/City.class.php
+++ Do_procedural_ao_orientado_a_objetos/nivel_7/class/City.class.php
@@ -1,22 +1,23 @@
 <?php
+    require_once('traits/Connection.trait.php');
 
     class City{
 
         use Connection{
             Connection::__construct as private __traitConstruct;
         }
-        
-        public function  __construct(){
-            $this__traitConstruct();
+
+        public function __construct(){
+            $this->__traitConstruct();
         }
 
-        public static function findAll(){
-            $query = 'SELECT nome FROM cidades';
+        public function findAll(){
+            $query = 'SELECT id, nome FROM cidades';
             $statement = $this->connection->prepare($query);
             $statement->execute();
             $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-            
             return $result;
         }
     }
+
 ?>
