e2f05b3aaa7a5ac91c26ecd42c20e578c4e7d0c6
adeonita <adeonita.sousa@gmail.com>
2020-05-10 20:53:56 -0300

    Implementaçao do autoload e validaçoes do recebimento de metodo e classe

--- Do_procedural_ao_orientado_a_objetos/nivel_7/index.php
+++ Do_procedural_ao_orientado_a_objetos/nivel_7/index.php
@@ -1,11 +1,62 @@
 <?php
-    require_once('class/PersonForm.class.php');
-    require_once('class/City.class.php');
+/**
+ require_once('class/Form.class.php');
+ require_once('class/Person.class.php');
+ require_once('class/City.class.php');
 
-    $form = new PersonForm();
-    $form->show();
+ 
 
-    $form = new City();
-    
-    
+ $person = new Person();
+// $person->showAll();
+ $form = new Form();
+ $form->show();
+
+ $form = new City();
+*/
+
+spl_autoload_register(function($fileName){
+    $path = "class/{$fileName}.class.php";
+    if(file_exists($path)){
+        require_once($path);
+    }
+});
+//Valida class
+if(isset($_REQUEST['class']) and !empty($_REQUEST['class']) and class_exists($_REQUEST['class'])){
+    $class = $_REQUEST['class'];
+    //$obj = new $class($_REQUEST);
+}
+//Valida Metodo
+if(isset($_REQUEST['method']) and !empty($_REQUEST['method']) and method_exists($class, $_REQUEST['method'])){
+    $method = $_REQUEST['method'];
+    //$obj->$method($_REQUEST);  
+}else{
+    $method = 'show';
+    //$obj->$method();  
+}
+
+
+$style = 'font-size: 42px;
+          text-align: center;
+          margin: 290px;';
+
+if($class and $method){
+    $obj = new $class($_REQUEST);
+    $obj->$method($_REQUEST);    
+}else{
+    print '<h1 style="'.$style.'">Alguem está perdido por aqui!</h1>';
+}
+
+
+
+
+/*$method = isset($_REQUEST['method']) ? $_REQUEST['method'] : 'show';
+
+if(class_exists($classe)){
+    $obj = new $classe($_REQUEST);
+    if(!empty($method) and (method_exists($classe, $method)) and $method == 'save'){
+        $obj->show();
+        $obj->$method($_REQUEST);
+    }
+}*/
+
 ?>
